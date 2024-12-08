b3d1e30fc7549cc5ad7c12017180117a64b1317c
Ellyse <ellyse7777@gmail.com>
2024-12-08 15:09:03 +0000

    part 1 solved

--- 2024/nix/day06/default.nix
+++ 2024/nix/day06/default.nix
@@ -1,6 +1,24 @@
 with builtins;
 with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/689fed12a013f56d4c4d3f612489634267d86529.tar.gz") {};
 let
+  sum = l: foldl' (acc: e: e + acc) 0 l;
+  
+  # how many times is c in the string s
+  countChar = s: c: 
+    builtins.foldl' 
+      (acc: i: 
+        if (builtins.substring i 1 s) == c then 
+          acc + 1 
+        else 
+          acc
+      ) 
+      0 
+      (
+        lib.range 
+          0 
+          ((builtins.stringLength s) - 1)
+      );
+    
   getGridFile = p: lib.readFile p;
 
   # type grid.
@@ -9,11 +27,71 @@ let
   getGrid = s:
     lib.init (lib.splitString "\n" s);
 
+  # direction is an attr set with x y increments ie {x=-1;y=0;} means left
+  # returns direction based on the guard character
+  getDirection = s:
+    let
+      dirMap = 
+        [
+          ["^" {x=0;    y=(-1);}]
+          [">" {x=1;    y=0;}]
+          ["v" {x=0;    y=1;}]
+          ["<" {x=(-1); y=0;}]
+        ];
+      match = builtins.filter (e: (builtins.head e) == s) dirMap;
+    in
+      builtins.elemAt (builtins.head match) 1; 
+
+  # pass in guard token, return new token that is 90 degree turn
+  turn90 = s:
+    let
+      m = builtins.filter (e: (builtins.head e) == s)  [ ["^" ">"] [">" "v"] ["v" "<"] ["<" "^"] ];
+    in 
+    builtins.elemAt (builtins.head m) 1;
+    
+  # returns the character (as string) in the grid at the passed in position
+  # x y must be in bounds or will throw exception
+  getXY = {x,y}: grid: 
+    let
+      row = builtins.elemAt grid y;
+    in
+      builtins.substring x 1 row;
+      
+  # places char into the grid, replacing existing character
+  putXY = {x,y}: c: grid:
+    foldl' 
+      (acc: e: 
+        if e == y then
+          (acc ++ [(replaceChar (builtins.elemAt grid e) x c)])
+        else
+          (acc ++ [(builtins.elemAt grid e)])
+        )
+      []
+      (lib.range 0 ((getGridHeight grid) - 1));
+    
+  replaceChar = s: index: c: 
+    let
+      len = builtins.stringLength s;
+      before = builtins.substring 0 index s;
+      after = builtins.substring (index + 1) (len - index) s;  
+    in
+      before + c + after;
+        
+  # adds pos + dir and returns a new position
+  moveXY = pos: dir:
+   {x = (pos.x + dir.x); y = (pos.y + dir.y); };
+    
+  getGridWidth = grid: builtins.stringLength (builtins.head grid);
+  getGridHeight = grid: builtins.length grid;
+  isValidPos = {x,y}: grid: 
+    (x >= 0) && (x < getGridWidth grid) && (y >= 0) && (y < getGridHeight grid);
+      
   # type gridstate, {grid, guardPos}
   getGridState = grid:
-    {
+    rec {
       inherit grid;
-      guardPos = getGuardPos(grid);
+      guardPos = getGuardPos grid;
+      guardInMap = (guardPos != -1);
     };
    
   # -1 if doesnt exist
@@ -41,16 +119,59 @@ let
   # returns where in this row the guard is, -1 means not found
   getGuardColumn = s:
     lib.findFirst
-      (i: (builtins.substring i 1 s) == "^")
+      (i: builtins.any (e: (builtins.substring i 1 s) == e) ["^" ">" "v" "<"])
       (-1)
       (lib.range 0 ((builtins.stringLength s) - 1));
+
+  # generate next state 
+  # returns gridState
+  tick = gridState: 
+    let
+      guard = getXY gridState.guardPos gridState.grid;
+      aheadDir = getDirection guard;
+      aheadPos = moveXY gridState.guardPos aheadDir;
+      aheadValid = isValidPos aheadPos gridState.grid;
+      isWall = aheadValid && ((getXY aheadPos gridState.grid) == "#");
+
+      # we move ahead if no wall, other we dont move (we turn right instead)
+      # allow walking off map
+      updatedGuardPos = if (!isWall) then aheadPos else gridState.guardPos;
+      updatedGuard = if isWall then turn90 guard else guard;
+      updatedGuardDir = getDirection updatedGuard;
+
+      updatedGrid = 
+        if updatedGuardPos != gridState.guardPos then
+          # we've moved, mark old position with X the put guard in new position
+          let
+            markedX = putXY gridState.guardPos "X" gridState.grid; 
+          in
+          putXY updatedGuardPos updatedGuard markedX
+        else
+          # we didnt move, update the guard marker
+          putXY updatedGuardPos updatedGuard gridState.grid;
+    in 
+      {grid = updatedGrid; guardPos = updatedGuardPos; guardInMap = (isValidPos updatedGuardPos gridState.grid);};
+
+  # calls ticks until the guard is no longer in the map
+  # returns a list of all the gridStates in reverse order
+  runTicks = gridState: prevGridStates: 
+    if (! gridState.guardInMap) then
+      prevGridStates
+    else
+      let
+        nextState = tick gridState;
+      in
+        trace "running tick...iteration=${builtins.toString (builtins.length prevGridStates)}"
+        (runTicks nextState ([nextState] ++ prevGridStates));  
 
   solvePart1 = p:
     let
       gridFile = getGridFile p;
       grid = getGrid gridFile;
-      guardPos = getGuardPos grid;
+      gridState = getGridState grid; 
+      ticks = runTicks gridState [];
+      finalGridState = builtins.head ticks; 
     in
-    guardPos;
+      sum (map (e: countChar e "X") finalGridState.grid);
 in
-  solvePart1 ./test_input
+ solvePart1 ./input
